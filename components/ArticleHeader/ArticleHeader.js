82965d4e3bb5e1f5f8dc7e9c89ab208df0cafb0b
datejer <ejer.priv@wp.pl>
2020-02-29 18:10:37 +0100

    Add rodzaju book

--- components/ArticleHeader/ArticleHeader.js
+++ components/ArticleHeader/ArticleHeader.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './ArticleHeader.scss';
 import Navigation from '../Navigation/Navigation';
 
-const ArticleHeader = ({ title }) => (
+const ArticleHeader = ({ title, img }) => (
   <>
     <div className="header">
-      <img src="https://cdn.pixabay.com/photo/2016/03/16/09/20/abraham-1260073_960_720.jpg" />
+      <img src={img} />
       <h1>{title}</h1>
     </div>
     <Navigation />
--- pages/rodzaju.js
+++ pages/rodzaju.js
@@ -5,21 +5,50 @@ import Header from '../components/ArticleHeader/ArticleHeader';
 
 const Rodzaju = () => (
   <Layout title="Księga Rodzaju">
-    <Header title="Księga Rodzaju" />
+    <Header title="Księga Rodzaju" img="https://i.imgur.com/w9UnyEw.jpg" />
     <div className="wrapper">
-      <q>Oto przykładowy cytat.</q>
-      <h2>Lorem Ipsum, fajne coś</h2>
+      <q>Wtedy Bóg rzekł: «Niechaj się stanie światłość!» I stała się światłość.</q>
+      <br />
+      <i>~Rdz 1,3</i>
+      <h2>Księga Rodzaju w pigułce</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ipsa eligendi, corrupti omnis
-        quia laudantium saepe libero dolorem ipsum possimus explicabo architecto necessitatibus id
-        eveniet cumque totam unde autem voluptatum perferendis quae cum! Qui, cupiditate accusamus
-        optio quidem voluptatum dolores minus et dolor vero, veniam, autem voluptate. Cupiditate
-        autem sunt doloribus consectetur dolorum esse veritatis deserunt. Sint laudantium placeat
-        minus est adipisci pariatur, aperiam, necessitatibus et a iure aspernatur dolores omnis
-        eveniet obcaecati culpa esse nam perferendis tempora praesentium soluta aut dicta. Velit
-        quis, odio, eligendi soluta aut nobis libero dignissimos facilis sint iusto fuga maxime,
-        delectus dicta praesentium sed.
+        <ul>
+          <li>Pierwsza księga zarówno Starego Testamentu, jak i tzw. Tory</li>
+          <li>Tradycyjnie autorstwo przypisuje się Mojżeszowi na przełomie XIII i XII w. p.n.e.</li>
+          <li>
+            Klasyczny tytuł pochodzi z języka greckiego („Genesis”) i jest wzorowany na hebrajskiej
+            nazwie księgi, pochodzącej od słowa „Bereshit”. Oznacza ono „na początku” i otwiera
+            księgę Rodzaju. Polski tytuł znajduje źródło w XV-wiecznym tłumaczeniu i pochodzi od
+            słowa „Rodzaj” lub „Ród”.
+          </li>
+        </ul>
       </p>
+      <h2>Pojęcia i definicje</h2>
+      <p>
+        <b>Tora</b> – inna nazwa na Pięcioksiąg Mojżeszowy, w którego skład wchodzi pięć pierwszych
+        ksiąg Starego Testamentu. Jego autorstwo tradycyjnie przypisuje się Mojżeszowi.
+      </p>
+      <h2>Księga Rodzaju współcześnie</h2>
+      <p>
+        Grupa Filmowa Darwin, stworzyła serię krótkometrażowych filmów p.t. „Wielkie Konflikty”,
+        przedstawiającą zatargi postaci znanych z literatury i popkultury. W wielu odcinkach
+        przedstawione zostały sceny z Biblii, również te z księgi Rodzaju.
+        <br />
+        <a href="https://www.youtube.com/watch?v=jiVb2HV6xBQ">Adam vs Wąż</a>
+        <br />
+        <a href="https://www.youtube.com/watch?v=KmGI_YZlYqw">Kain vs Abel</a>
+        <br />
+        <a href="https://www.youtube.com/watch?v=6MjYtBakc30">Noe vs Bóg</a>
+      </p>
+      <i>
+        Bibliografia:
+        <br />
+        Pochodzenie nazwy „Genesis” – Wikipedia The Free Encyclopedia,{' '}
+        <a href="https://en.wikipedia.org/wiki/Book_of_Genesis">Book of Genesis</a>
+        <br />
+        Wersety zostały zaczerpnięte z <a href="https://biblia.deon.pl/">Biblii Tysiąclecia</a>.
+        wyd. Pallottinum w Poznaniu, 2003
+      </i>
     </div>
   </Layout>
 );
